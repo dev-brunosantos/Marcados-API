9d3f398e90dccee8f0031e73d154f008ce7943c6
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-10-16 23:44:28 -0300

    Melhorando a escrita do codigo

--- src/services/CargosServices.ts
+++ src/services/CargosServices.ts
@@ -12,27 +12,27 @@ class CargosServices {
 
         if(!cargoNome) {
             const criar = await cargos.create({ data: { cargo }})
-            return { status: "Cargo criado com sucesso."}
+            return "Cargo criado com sucesso."
         }
-        return { erro: "O cargo informado já existe."}
+        return "O cargo informado já existe."
     }
 
     async ListarCargos() {
         const listar = await cargos.findMany()
         if(!listar) {
-            return { erro: "Não existe nenhum cargo cadastrado no sistema."}
+            return "Não existe nenhum cargo cadastrado no sistema."
         }
-        return { listar }
+        return listar
     }
 
     async BuscarCargo({ cargo }:CargosDados) {
         const cargoNome = await cargos.findFirst({ where: { cargo }})
 
         if(cargoNome) {
-            return { cargoNome }
+            return cargoNome 
         }
 
-        return { erro: "O cargo informado não esta cadastrado no sistema."}
+        return "O cargo informado não esta cadastrado no sistema."
     }
 }
 

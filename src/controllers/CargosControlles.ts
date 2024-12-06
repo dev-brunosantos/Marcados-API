675bb7bb711fb09928ee888e854ad2179d6995b2
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-06 13:58:00 -0300

    Adicionando os metodos no controllers de Cargos

--- src/controllers/CargosControlles.ts
+++ src/controllers/CargosControlles.ts
@@ -13,10 +13,34 @@ class Cargos {
         const cargos = await services.ListarCargos()
         return res.json(cargos)
     }
-    async BuscarCargo(req: Request, res: Response) {
-        const { cargo } = req.params
-        const cargoNome = await services.BuscarCargo({ cargo })
+    async BuscarCargoId(req: Request, res: Response) {
+        const { id } = req.params
+        const cargoNome = await services.BuscarCargoId(Number(id))
         return res.json(cargoNome)
+    }
+    // async BuscarCargo(req: Request, res: Response) {
+    //     const { cargo } = req.query
+    //     if (!cargo || typeof cargo !== 'string') {
+    //         return res.status(400).json({ error: "O parâmetro 'cargo' é obrigatório e deve ser uma string." });
+    //     }
+
+    //     let teste:string = cargo
+
+    //     const cargoNome = await services.BuscarCargo(teste)
+    //     return res.json(cargoNome)
+    // }
+
+    async EditarCargo(req: Request, res: Response) {
+        const { id } = req.params
+        const { cargo } = req.body
+        const editar = await services.EditarCargo(Number(id), cargo)
+        res.json(editar)
+    }
+
+    async ApagarCargo(req: Request, res: Response) {
+        const { id } = req.params
+        const apagar = await services.ApagarCargo(Number(id))
+        res.json(apagar)
     }
 }
 

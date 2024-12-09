95e7340a9153b34f52b922aeb013ddbb3b4010fb
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-09 19:25:42 -0300

    Correção na logica da função

--- src/functions/formata_data.ts
+++ src/functions/formata_data.ts
@@ -1,4 +1,5 @@
-export function FormataData(data: Date): string[] {
-    let dataFormatada = data.toISOString().slice(0, 10).split("-")
+export function FormataData(data: Date): string {
+    let dataInicial = data.toISOString().slice(0, 10).split("-")
+    let dataFormatada = dataInicial[2] + "/" + dataInicial[1] + "/" + dataInicial[0]
     return dataFormatada
 }

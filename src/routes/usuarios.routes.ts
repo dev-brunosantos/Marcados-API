e86921c30b79c30f4021ad4899e343b9e94bac68
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-10 12:15:59 -0300

    Atualizando as rotas e o endpoint de teste

--- src/routes/usuarios.routes.ts
+++ src/routes/usuarios.routes.ts
@@ -9,6 +9,6 @@ UsuarioRotas.post(`/${usuarioHttp}`, UsuarioController.Cadastrar)
 UsuarioRotas.get(`/${usuarioHttp}`, UsuarioController.Listar) 
 UsuarioRotas.get(`/${usuarioHttp}/usuario_id`, UsuarioController.UsuarioId) 
 UsuarioRotas.post(`/${usuarioHttp}/:nome`, UsuarioController.UsuarioNome) 
-UsuarioRotas.patch(`/${usuarioHttp}/:id`, UsuarioController.EditarUsuario) 
+UsuarioRotas.patch(`/${usuarioHttp}/editar/:id`, UsuarioController.EditarUsuario) 
 
 export { UsuarioRotas }

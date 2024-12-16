d86711e7af7584dbaa2d4484e21e1dade1c97b10
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-16 11:25:19 -0300

    Refatorando o codigo

--- src/services/Escalas.service.ts
+++ src/services/Escalas.service.ts
@@ -1,17 +1,6 @@
 import { FiltrarIntegrantes } from "../functions/filtrar_integrantes";
 import { filtrarNaipe } from "../functions/filtrar_naipe_escala";
 
-// function filtrarNaipe(naipe: string[]):string[] {
-//     let pessoa1 = Math.floor(Math.random() * naipe.length)
-//     let pessoa2 = Math.floor(Math.random() * naipe.length)
-    
-//     if(pessoa2 === pessoa1) {
-//         pessoa2 = Math.floor(Math.random() * naipe.length)
-//     }
-//     return [naipe[pessoa1], naipe[pessoa2]]
-// }
-
-
 class EscalasServices {
     async CriarEscala() {
         try {

8cbe1dc376a0082dc25ea9d8d3f705fac1056a7e
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-16 14:27:18 -0300

    Criando rotas de listagem de escalas

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
 import { routes } from './routes/routes'
 import { EscalasController } from './controllers/Escalas.controller'
+import { EscalasServices } from './services/Escalas.service'
 
 const app = express()
 
@@ -13,5 +14,6 @@ app.use(routes.NaipeRotas)
 app.use(routes.UsuarioRotas)
 
 app.post('/escalas', EscalasController.CriarEscalas)
+app.get('/escalas', EscalasController.ListarEscalas)
 
 export { app }

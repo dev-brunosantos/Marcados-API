f117c77830afa40031dcf37b1b954afe7798ce2a
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-16 13:40:28 -0300

    Adicionando a função de criação de escalas no metodo CRIAR ESCALAS

--- src/services/Escalas.service.ts
+++ src/services/Escalas.service.ts
@@ -1,49 +1,29 @@
 import { prismaConfig } from "../config/prismaConfig";
-import { FiltrarIntegrantes, FiltrarMinistros } from "../functions/filtrar_integrantes";
-import { filtrarNaipe } from "../functions/filtrar_naipe_escala";
+import { criarEscala } from "../functions/criar_escala";
 
 const { escalas } = prismaConfig;
 
 class EscalasServices {
     async CriarEscala() {
         try {
-            const ministro = await FiltrarMinistros("Ministro")
+            let dados = await criarEscala()
+            const { 
+                ministro, sopranos, contraltos, tenores, tecladistas, 
+                violao, guitarrista, baixista, baterista 
+            } = dados;
 
-            const soprano = await FiltrarIntegrantes("Soprano")
-            const contralto = await FiltrarIntegrantes("Contralto")
-            const tenor = await FiltrarIntegrantes("Tenor")
-
-            const teclado = await FiltrarIntegrantes("Teclado")
-            const violao = await FiltrarIntegrantes("Violão")
-            const guitar = await FiltrarIntegrantes("Guitarra")
-            const baixo = await FiltrarIntegrantes("Baixo")
-            const bateria = await FiltrarIntegrantes("Bateria")
-
-            if (soprano && contralto && tenor) {
-
-                const dados = {
-                    ministro: ministro[Math.floor(Math.random() * ministro.length)],
-                    sopranos: filtrarNaipe(soprano),
-                    contraltos: filtrarNaipe(contralto),
-                    tenores: filtrarNaipe(tenor),
-                    tecladistas: filtrarNaipe(teclado),
-                    violao: violao[Math.floor(Math.random() * violao.length)],
-                    guitarrista: guitar[Math.floor(Math.random() * guitar.length)],
-                    baixista: baixo[Math.floor(Math.random() * baixo.length)],
-                    baterista: bateria[Math.floor(Math.random() * bateria.length)]
-                }
-
+            if (dados) {
                 return await escalas.create({
                     data: {
-                        ministro: dados.ministro,
-                        soprano: dados.sopranos,
-                        contralto: dados.contraltos,
-                        tenor: dados.tenores,
-                        teclado: dados.tecladistas,
-                        violao: dados.violao,
-                        guitarra: dados.guitarrista,
-                        baixo: dados.baixista,
-                        bateria: dados.baterista
+                        ministro: ministro,
+                        soprano: sopranos,
+                        contralto: contraltos,
+                        tenor: tenores,
+                        teclado: tecladistas,
+                        violao: violao,
+                        guitarra: guitarrista,
+                        baixo: baixista,
+                        bateria: baterista
                     }
                 })
             }
